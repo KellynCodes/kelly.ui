b5ca474de6e7591c8d24cc202535a4c13e8e41a8
kellyncodes <kellyncodes@gmail.com>
2023-09-01 01:27:05 +0100

    fix: web worker warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent implements OnInit {
   }
 }
 
-/*if (typeof Worker !== 'undefined') {
+if (typeof Worker !== 'undefined') {
   // Create a new
   const worker = new Worker(new URL('./app.worker', import.meta.url));
   worker.onmessage = ({ data }) => {
@@ -44,4 +44,4 @@ export class AppComponent implements OnInit {
 } else {
   // Web Workers are not supported in this environment.
   // You should add a fallback so that your program still executes correctly.
-}*/
+}
