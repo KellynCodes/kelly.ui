190eb1ff9da54b1a062f1c900a5ae65da029b3c0
kellyncode <eng.kelechiamos@gmail.com>
2023-09-23 18:35:03 +0100

    fix: server url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5299/api/v1',
+  //apiUrl: 'http://localhost:5299/api/v1',
+  apiUrl: 'https://api-kelly.onrender.com/api/v1'
 };
